e0b79a776f3893bca24121b7868ba50f10319631
Stoff Audio LLC <94483749+StoffAudioLLC@users.noreply.github.com>
2021-11-18 12:36:38 -0500

    V1.0.1 resource files

---
+++ JuceLibraryCode/BinaryData.h
@@ -0,0 +1,45 @@
+/* =========================================================================================
+
+   This is an auto-generated file: Any edits you make may be overwritten!
+
+*/
+
+#pragma once
+
+namespace BinaryData
+{
+    extern const char*   Gain_knobnoshad01_png;
+    const int            Gain_knobnoshad01_pngSize = 88195;
+
+    extern const char*   pedal_back_10_2x100_jpg;
+    const int            pedal_back_10_2x100_jpgSize = 815466;
+
+    extern const char*   Gain_knoblayerslight_png;
+    const int            Gain_knoblayerslight_pngSize = 77772;
+
+    extern const char*   donatebuthover_png;
+    const int            donatebuthover_pngSize = 4834;
+
+    extern const char*   Gain_knoblayersshadow2_png;
+    const int            Gain_knoblayersshadow2_pngSize = 101223;
+
+    extern const char*   donatebut_png;
+    const int            donatebut_pngSize = 4821;
+
+    // Number of elements in the namedResourceList and originalFileNames arrays.
+    const int namedResourceListSize = 6;
+
+    // Points to the start of a list of resource names.
+    extern const char* namedResourceList[];
+
+    // Points to the start of a list of resource filenames.
+    extern const char* originalFilenames[];
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding data and its size (or a null pointer if the name isn't found).
+    const char* getNamedResource (const char* resourceNameUTF8, int& dataSizeInBytes);
+
+    // If you provide the name of one of the binary resource variables above, this function will
+    // return the corresponding original, non-mangled filename (or a null pointer if the name isn't found).
+    const char* getNamedResourceOriginalFilename (const char* resourceNameUTF8);
+}
